bd49de1d3afbbc4501e626c5383d8ea7c2fc50fb
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2020-09-28 11:40:41 +0600

    view details col fixed

--- js/myscript.js
+++ js/myscript.js
@@ -5,32 +5,9 @@
   });
 
   //   for datatable
-  $(document).ready(function() {
-      var table = $('#example').DataTable();
-  });
-  // for form
-  /* Scroll to Top */
   //   $(document).ready(function() {
-  //       $(window).scroll(function() {
-  //           if ($(this).scrollTop() > 50) {
-  //               $('#scroll-to-top').fadeIn();
-  //           } else {
-  //               $('#scroll-to-top').fadeOut();
-  //           }
-  //       });
-
-  //       $('#scroll-to-top').click(function() {
-  //           $('#scroll-to-top').tooltip('hide');
-  //           $('body,html').animate({
-  //               scrollTop: 0
-  //           }, 800);
-  //           return false;
-  //       });
-
-  //       $('#scroll-to-top').tooltip('show');
-
+  //       var table = $('#example').DataTable();
   //   });
-
 
   // scrool to top by sayket
 
@@ -55,9 +32,9 @@
   }
 
 
-  $(document).ready(function() {
-      $('#example').DataTable();
-  });
+  //   $(document).ready(function() {
+  //       $('#example').DataTable();
+  //   });
 
   // for select2
   //   $(document).ready(function() {
@@ -72,7 +49,6 @@
           document.getElementById(iconID).className = "fa fa-chevron-circle-up";
       }
   }
-
 
 
   $('.input-group').datepicker({
